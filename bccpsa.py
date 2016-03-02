a176a9b811bad6e1a5f827ac45e9ccdd3d62cd85
Mukandila Mukandila <mukandila.m@gmail.com>
2016-03-02 13:18:36 -0500

    Some changes. Hope they work!

--- bccpsa.py
+++ bccpsa.py
@@ -1,46 +1,34 @@
 import gphoto2 as gp
 import sys
 
-
-def _lookup_widget(key, child):
-    ret = gp.gp_widget_get_child_by_name(child, key)
-
-    if ret < gp.GP_OK:
-        ret = gp.gp_widget_get_child_by_label(child, key)
-
-    return ret
-
-def get_config_value_string(camera, key, value, context):
-    #child = gp._widget
-    child = gp.gp_camera_get_config(camera, context)
-    if child < gp.GP_OK:
-        print "camera_get_config failed: %d" + child
-
-    #ret = _lookup_widget(child, key)
-    #if ret < gp.GP_OK:
-    #    print "lookup widget failed: %d" + ret
-
-    ret = gp.gp_widget_get_value(value)
-    if ret < gp.GP_OK:
-        print "could not query widget value: %d" + ret
-
-
 def main():
     context = gp.gp_context_new()
     camera = gp.check_result(gp.gp_camera_new())
     gp.check_result(gp.gp_camera_init(camera, context))
+    
+    # Get config widget: this returns a child to be used...
+    config_widget = gp.check_result(gp.gp_camera_get_config(camera, context))
 
-    #config = gp.check_result(gp.gp_camera_get_config(camera, context))
-
+    # Get all children
+    children_count = gp.check_result(gp.gp_widget_count_children(config_widget))
+    if children_count < 1:
+        print "No children!"
+    
+    # Cycling through all available children and printing their names. Hope this works!
+    for n in range(children_count):
+        child = gp.check_result(gp.gp_widget_get_child(config_widget, n))
+        name = gp.check_result(gp.gp_widget_get_name(child))
+        print name
+    
     #print config
 
-    iso = ''
+    #iso = ''
 
-    ret = get_config_value_string(camera, "iso", iso, context)
-    if ret < gp.GP_OK:
-        print "Unable to get ISO: %d" + ret
-    else:
-        print "ISO is: " + iso
+    #ret = get_config_value_string(camera, "iso", iso, context)
+    #if ret < gp.GP_OK:
+    #    print "Unable to get ISO: %d" + ret
+    #else:
+    #    print "ISO is: " + iso
 
 if __name__  == "__main__":
     sys.exit(main())
