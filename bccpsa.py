e20fa2b0b71fef0913e580221fe0db682912eb7c
M. Mukandila <ndila@muka.io>
2016-03-02 18:30:16 -0500

    Added function to get basic config settings on camera.

--- bccpsa.py
+++ bccpsa.py
@@ -1,34 +1,26 @@
 import gphoto2 as gp
 import sys
 
+def get_config_value(value, camera, context):
+
+    config_widget = gp.check_result(gp.gp_camera_get_config(camera, context))
+    camera_widget_child = gp.check_result(gp.gp_widget_get_child_by_name(config_widget, value))
+    config_value = gp.check_result(gp.gp_widget_get_value(camera_widget_child))
+
+    return config_value
+
 def main():
+
+    #Initialize the camera
     context = gp.gp_context_new()
     camera = gp.check_result(gp.gp_camera_new())
     gp.check_result(gp.gp_camera_init(camera, context))
-    
-    # Get config widget: this returns a child to be used...
-    config_widget = gp.check_result(gp.gp_camera_get_config(camera, context))
 
-    # Get all children
-    children_count = gp.check_result(gp.gp_widget_count_children(config_widget))
-    if children_count < 1:
-        print "No children!"
-    
-    # Cycling through all available children and printing their names. Hope this works!
-    for n in range(children_count):
-        child = gp.check_result(gp.gp_widget_get_child(config_widget, n))
-        name = gp.check_result(gp.gp_widget_get_name(child))
-        print name
-    
-    #print config
+    #Get basic info from camera
+    print "ISO: " + get_config_value("iso", camera, context)
+    print "Shutter Speed: " + get_config_value("shutterspeed2", camera, context)
+    print "Aperture: " + get_config_value("f-number", camera, context)
 
-    #iso = ''
-
-    #ret = get_config_value_string(camera, "iso", iso, context)
-    #if ret < gp.GP_OK:
-    #    print "Unable to get ISO: %d" + ret
-    #else:
-    #    print "ISO is: " + iso
 
 if __name__  == "__main__":
     sys.exit(main())
